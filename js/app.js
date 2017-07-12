861c84cddb1f521435b0f32cedf9cf760654b1c4
Rami Bououni <rami.bououni@gmail.com>
2017-07-12 11:04:46 -0700

    finished js code

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
 'strict mode'
 
-function Portfolio(name, image, link, description){
-  this.name = name,
-  this.image = image,
-  this.link = link,
-  this.description = description
+var articles = [];
+
+function Portfolio(portfolioObj){
+  this.title = portfolioObj.title,
+  this.image = portfolioObj.image,
+  this.link = portfolioObj.link,
+  this.description = portfolioObj.description;
 }
+
+Portfolio.prototype.toHtml = function() {
+
+  var $newArticle = $('article.template').clone();
+  $newArticle.removeClass('template');
+
+  $newArticle.find('h1').text(this.title);
+  $newArticle.append(this.image);
+  $newArticle.find('a').text('Link to my Github').attr('href', this.link);
+  $newArticle.find('p').html(this.description);
+
+
+  $newArticle.append('<hr>');
+  return $newArticle;
+};
+
+
+portfolioObjets.forEach(function(articleObject) {
+  // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
+  articles.push(new Portfolio(articleObject));
+});
+
+articles.forEach(function(article) {
+  $('#articles').append(article.toHtml());
+});
